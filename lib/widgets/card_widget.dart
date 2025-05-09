53b4c79563fc71999e2cdad0f8465b1934df4e27
Mohsen faraji <mohsen.faraji.dev@gmail.com>
2025-05-09 14:56:15 +0330

    جداکردن ویجت کارد

---
+++ lib/widgets/card_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:persian_hokm/models/card.dart';
+
+class CardWidget extends StatelessWidget {
+  final GameCard card;
+
+  const CardWidget({super.key, required this.card});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).height * 0.15,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.black, width: 1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 1,
+            offset: const Offset(3, 3),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Image.asset(
+          card.imagePath,
+          fit: BoxFit.cover,
+          errorBuilder: (context, error, stackTrace) {
+            return Container(
+              color: Colors.white,
+              child: Center(
+                child: Text(
+                  '${card.rankName}\n${card.suitSymbol}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
